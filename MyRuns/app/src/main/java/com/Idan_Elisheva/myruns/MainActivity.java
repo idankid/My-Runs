36b13f84ced8f139754eec7f8bed43e00649f1b6
Idan Kideckel <idanbkideckel@gmail.com>
2023-03-26 12:24:28 +0300

    created

---
+++ MyRuns/app/src/main/java/com/Idan_Elisheva/myruns/MainActivity.java
@@ -0,0 +1,14 @@
+package com.Idan_Elisheva.myruns;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
