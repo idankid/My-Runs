41b78266ea85f00386e38307ba48d6ba070c2b92
Idan Kideckel <idanbkideckel@gmail.com>
2023-03-26 14:20:12 +0300

    update

--- MyRuns/app/src/main/java/com/idank_elishevaa/myruns/SettingsActivity.java
+++ MyRuns/app/src/main/java/com/idank_elishevaa/myruns/SettingsActivity.java
@@ -3,14 +3,12 @@ package com.idank_elishevaa.myruns;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button start = (Button) findViewById(R.id.start);
+        setContentView(R.layout.activity_settings);
     }
 }
