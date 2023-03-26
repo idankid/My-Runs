71cfbbe25e8878b2677338d1c85fd73496d388ed
Idan Kideckel <idanbkideckel@gmail.com>
2023-03-26 14:36:34 +0300

    added map

--- MyRuns/app/src/main/java/com/idank_elishevaa/myruns/MainActivity.java
+++ MyRuns/app/src/main/java/com/idank_elishevaa/myruns/MainActivity.java
@@ -2,7 +2,9 @@ package com.idank_elishevaa.myruns;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button start = (Button) findViewById(R.id.start);
+        Intent goToRun  = new Intent(this, MapActivity.class);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(goToRun);
+            }
+        });
     }
 }
---
+++ MyRuns/app/src/main/java/com/idank_elishevaa/myruns/MapActivity.java
@@ -0,0 +1,14 @@
+package com.idank_elishevaa.myruns;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
