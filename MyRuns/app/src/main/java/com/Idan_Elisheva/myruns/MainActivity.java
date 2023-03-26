36913b7f2d04abb597032ad2c2b4b562e80250d4
Idan Kideckel <idanbkideckel@gmail.com>
2023-03-26 13:24:53 +0300

    Update MainActivity.java

--- MyRuns/app/src/main/java/com/Idan_Elisheva/myruns/MainActivity.java
+++ MyRuns/app/src/main/java/com/Idan_Elisheva/myruns/MainActivity.java
@@ -2,8 +2,12 @@ package com.Idan_Elisheva.myruns;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -23,10 +27,34 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-//
-//    button.set
-//    public void onClick(View v){
-//        Intent jump = new Intent()
-//
-//    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        super.onCreateOptionsMenu(menu);
+        MenuItem about = menu.add("Exit");
+        MenuItem settings = menu.add("Settings");
+        Intent goToSettings = new Intent(this, SettingsActivity.class);
+        settings.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener()
+        {
+            @Override
+            public boolean onMenuItemClick(MenuItem item)
+            {
+                startActivity(goToSettings);
+                return true;
+            }
+        });
+//        about.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener()
+//        {
+//            @Override
+//            public boolean onMenuItemClick(MenuItem item)
+//            {
+//                String str = "DEvice OS: Android" + Build.VERSION.RELEASE +"API " + Build.VERSION.SDK_INT;
+//                AlertDialog.Builder dialog = new AlertDialog.Builder();
+//                dialog.setMessage(str);
+//                return true;
+//            }
+//        });
+        return true;
+    }
+
 }
